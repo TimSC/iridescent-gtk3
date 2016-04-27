982755230c3939b8dd99c853278f176e1f9b66c0
Tim <tim2009@sheerman-chase.org.uk>
2016-04-27 16:41:03 +0100

    Override request size methods

--- iridescent-map.cpp
+++ iridescent-map.cpp
@@ -3,9 +3,6 @@
 
 G_DEFINE_TYPE( IridescentMap, iridescent_map, GTK_TYPE_DRAWING_AREA )
 
-static void iridescent_map_class_init( IridescentMapClass* klass )
-    {}
-
 static void iridescent_map_init( IridescentMap* self )
 {
     GdkRGBA c;
@@ -17,3 +14,27 @@ static void iridescent_map_init( IridescentMap* self )
     gtk_widget_override_background_color( widget, GTK_STATE_FLAG_NORMAL, &c );
 }
 
+void iridescent_map_get_preferred_height(GtkWidget *widget,
+                                         gint *minimum_height,
+                                         gint *natural_height)
+{
+	*minimum_height = 10;
+	*natural_height = 20;
+}
+
+void iridescent_map_get_preferred_width(GtkWidget *widget,
+                                         gint *minimum_width,
+                                         gint *natural_width)
+{
+	*minimum_width = 50;
+	*natural_width = 100;
+}
+
+static void iridescent_map_class_init( IridescentMapClass* klass )
+{
+	GtkWidgetClass *widget_class = (GtkWidgetClass*) klass;
+	widget_class->get_preferred_height = iridescent_map_get_preferred_height;
+	widget_class->get_preferred_width = iridescent_map_get_preferred_width;
+}
+
+//gtk_widget_get_preferred_size
