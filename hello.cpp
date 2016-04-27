eca3f8635c9ce171ee59296ca38f77475588260b
Tim <tim2009@sheerman-chase.org.uk>
2016-04-27 18:07:42 +0100

    Do custom drawing in widget

--- hello.cpp
+++ hello.cpp
@@ -11,11 +11,11 @@ activate (GtkApplication* app,
 	gtk_window_set_title (GTK_WINDOW (window), "Window");
 	gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
 
-	GtkWidget *button_box = gtk_button_box_new (GTK_ORIENTATION_HORIZONTAL);
+	GtkWidget *button_box = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 0);
 	gtk_container_add (GTK_CONTAINER (window), button_box);
 
 	GtkWidget *test = GTK_WIDGET(g_object_new(IRIDESCENT_MAP_TYPE,NULL));
-	gtk_container_add (GTK_CONTAINER (button_box), test);
+	gtk_box_pack_start (GTK_BOX(button_box), test, TRUE, TRUE, 0);
 
 	gtk_widget_show_all (window);
 }
--- iridescent-map.cpp
+++ iridescent-map.cpp
@@ -1,5 +1,7 @@
 #include <gtk/gtk.h>
 #include "iridescent-map.h"
+#include <iostream>
+using namespace std;
 
 G_DEFINE_TYPE( IridescentMap, iridescent_map, GTK_TYPE_DRAWING_AREA )
 
@@ -30,11 +32,33 @@ void iridescent_map_get_preferred_width(GtkWidget *widget,
 	*natural_width = 100;
 }
 
+gboolean iridescent_map_draw(GtkWidget *widget,
+                                cairo_t *cr)
+{
+	GtkAllocation allocation;
+	gtk_widget_get_allocation (widget, &allocation);
+
+	cairo_save(cr);
+
+	cairo_set_source_rgba(cr, 1.0, 0.0, 0.0, 1.0);
+	cairo_set_line_width(cr, 5.0);
+	cairo_move_to(cr, 0.0, 
+				0.0);
+	cairo_line_to(cr, allocation.width, 
+				allocation.height);
+
+	cairo_stroke(cr);
+
+	cairo_restore(cr);
+
+	return true; //stop other handlers from being invoked for the event
+}
+
 static void iridescent_map_class_init( IridescentMapClass* klass )
 {
 	GtkWidgetClass *widget_class = (GtkWidgetClass*) klass;
 	widget_class->get_preferred_height = iridescent_map_get_preferred_height;
 	widget_class->get_preferred_width = iridescent_map_get_preferred_width;
+	widget_class->draw = iridescent_map_draw;
 }
 
-//gtk_widget_get_preferred_size
