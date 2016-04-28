d28a71ce3b3c761714a0c3b1df27fe40bc13fbe0
Tim <tim2009@sheerman-chase.org.uk>
2016-04-28 11:26:43 +0100

    Compile map code into widget

--- iridescent-map.cpp
+++ iridescent-map.cpp
@@ -1,7 +1,17 @@
 #include <gtk/gtk.h>
 #include "iridescent-map.h"
 #include <iostream>
+
+#include "iridescent-map/LabelEngine.h"
+#include "iridescent-map/Regrouper.h"
+#include "iridescent-map/ReadInput.h"
+#include "iridescent-map/Transform.h"
+#include "iridescent-map/drawlib/drawlibcairo.h"
+#include "iridescent-map/MapRender.h"
+
 using namespace std;
+
+typedef map<int, map<int, LabelsByImportance> > OrganisedLabelsMap;
 
 G_DEFINE_TYPE( IridescentMap, iridescent_map, GTK_TYPE_DRAWING_AREA )
 
@@ -16,6 +26,68 @@ static void iridescent_map_init( IridescentMap* self )
     widget = GTK_WIDGET(self);
 
     gtk_widget_override_background_color( widget, GTK_STATE_FLAG_NORMAL, &c );
+
+	// ** Collect labels from off screen tiles
+	OrganisedLabelsMap organisedLabelsMap;
+	cairo_surface_t *offScreenSurface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, 640, 640);
+
+	for(int x=2034; x <= 2036; x++)
+		for(int y=1373; y<= 1375; y++)
+		{
+			if(x == 2035 && y == 1374) continue;
+			FeatureStore featureStore;
+			ReadInput(12, x, y, featureStore);
+			class SlippyTilesTransform slippyTilesTransform(12, x, y);
+
+			class DrawLibCairoPango drawlib(offScreenSurface);	
+			class MapRender mapRender(&drawlib);
+			LabelsByImportance organisedLabels;
+			
+			mapRender.Render(12, featureStore, false, true, slippyTilesTransform, organisedLabels);
+
+			OrganisedLabelsMap::iterator it = organisedLabelsMap.find(x);
+			if(it == organisedLabelsMap.end())
+				organisedLabelsMap[x] = map<int, LabelsByImportance>();
+			map<int, LabelsByImportance> &col = organisedLabelsMap[x];
+			col[y] = organisedLabels;
+		}
+
+	cairo_surface_destroy(offScreenSurface);
+
+	// ** Render without labels and collect label info **
+
+	cairo_surface_t *surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, 640, 640);
+	FeatureStore featureStore;
+	ReadInput(12, 2035, 1374, featureStore);
+
+	class SlippyTilesTransform slippyTilesTransform(12, 2035, 1374);
+	//class SlippyTilesTransform slippyTilesTransform(14, 8143, 5498);
+
+	class DrawLibCairoPango drawlib(surface);	
+	class MapRender mapRender(&drawlib);
+	LabelsByImportance organisedLabels;
+	
+	mapRender.Render(12, featureStore, true, true, slippyTilesTransform, organisedLabels);
+	organisedLabelsMap[2035][1374] = organisedLabels;
+
+	// ** Render labels **
+	RenderLabelList labelList;
+	RenderLabelListOffsets labelOffsets;
+	for(int y=1373; y<= 1375; y++)
+	{
+		for(int x=2034; x <= 2036; x++)
+		{
+			map<int, LabelsByImportance> &col = organisedLabelsMap[x];
+			labelList.push_back(col[y]);
+			labelOffsets.push_back(std::pair<double, double>(640.0*(x-2035), 640.0*(y-1374)));
+		}
+	}
+
+	mapRender.RenderLabels(labelList, labelOffsets);
+
+	//cairo_surface_write_to_png(surface, "image.png");	
+	cairo_surface_destroy(surface);
+
 }
 
 void iridescent_map_get_preferred_height(GtkWidget *widget,
